3b3fbef9d3e8c691cdc319f93b6de2a7ed7fc8f1
Erik Pastuszek <erikpastuzek@gmail.com>
2022-12-15 02:21:56 -0300

    added auth and user verification

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,30 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const {
+	models: { User },
+} = require('../sequelize');
 module.exports = {
 	async login(req, res, next) {
 		try {
 			const { username, password } = req.body;
-			// Verify that the user exists
-			const user = { username, password };
-			const accessToken = jwt.sign({ user }, process.env.JWT_ACCESS_TOKEN, {
-				expiresIn: '30s',
+			const finded = await User.findOne({ where: { username } });
+			if (!finded) return res.status(404).json({ message: 'User not found' });
+			let hash = finded.password;
+			bcrypt.compare(password, hash, function (err, result) {
+				if (err) return res.status(500).json({ error: err });
+				if (!result)
+					return res
+						.status(401)
+						.json({ message: 'Unauthorized, password is incorrect' });
+				// Verify that the user exists
+				const user = { username, password };
+				const accessToken = jwt.sign({ user }, process.env.JWT_ACCESS_TOKEN, {
+					expiresIn: '30m',
+				});
+				res
+					.header('authorization', accessToken)
+					.json({ accessToken, message: 'User authenticated' });
 			});
-			res
-				.header('authorization', accessToken)
-				.json({ accessToken, message: 'User authenticated' });
 		} catch (e) {
 			next(e);
 		}

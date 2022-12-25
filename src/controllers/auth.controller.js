ac8e22510e76fa2e3785c8f4ed19bc71181fbb37
Erik Pastuszek <erikpastuzek@gmail.com>
2022-12-25 00:59:58 -0300

    improved auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,14 +8,24 @@ module.exports = {
 		try {
 			const { username, password } = req.body;
 			const finded = await User.findOne({ where: { username } });
-			if (!finded) return res.status(404).json({ message: 'User not found' });
+
+			if (!finded) {
+				return res
+					.status(404)
+					.json({ error: true, message: 'User not found.' });
+			}
 			let hash = finded.password;
 			bcrypt.compare(password, hash, function (err, result) {
-				if (err) return res.status(500).json({ error: err });
-				if (!result)
-					return res
-						.status(401)
-						.json({ message: 'Unauthorized, password is incorrect' });
+				if (err) {
+					return res.status(500).json({ error: true, message: err });
+				}
+				if (!result) {
+					return res.status(401).json({
+						error: true,
+						message: 'Unauthorized, password is incorrect.',
+					});
+				}
+
 				// Verify that the user exists
 				const user = { username, password };
 				const accessToken = jwt.sign(
@@ -27,7 +37,7 @@ module.exports = {
 				);
 				res
 					.header('authorization', accessToken)
-					.json({ accessToken, message: 'User authenticated' });
+					.json({ accessToken, message: 'User authenticated.' });
 			});
 		} catch (e) {
 			next(e);
